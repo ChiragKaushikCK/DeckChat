a3cbecef0cdaa655e4637a69a1b06659345c142a
Chirag Kaushik <129536554+ChiragKaushikCK@users.noreply.github.com>
2026-03-03 17:40:05 +0530

    Update import statements for langchain modules

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
 
 # -----------------------------
 # Page Config & Styles
